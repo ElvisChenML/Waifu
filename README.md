a286a3f87fe41133d0895cd2a30189d3dc1ec62e
ElvisChenML <42546145+ElvisChenML@users.noreply.github.com>
2024-05-24 11:52:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# HelloPlugin
+
+<!--
+## 插件开发者详阅
+
+### 开始
+
+此仓库是 QChatGPT 插件模板，您可以直接在 GitHub 仓库中点击右上角的 "Use this template" 以创建你的插件。  
+接下来按照以下步骤修改模板代码：
+
+#### 修改模板代码
+
+- 修改此文档顶部插件名称信息
+- 将此文档下方的`<插件发布仓库地址>`改为你的插件在 GitHub· 上的地址
+- 补充下方的`使用`章节内容
+- 修改`main.py`中的`@register`中的插件 名称、描述、版本、作者 等信息
+- 修改`main.py`中的`MyPlugin`类名为你的插件类名
+- 将插件所需依赖库写到`requirements.txt`中
+- 根据[插件开发教程](https://github.com/RockChinQ/QChatGPT/wiki/7-%E6%8F%92%E4%BB%B6%E5%BC%80%E5%8F%91)编写插件代码
+- 删除 README.md 中的注释内容
+
+
+#### 发布插件
+
+推荐将插件上传到 GitHub 代码仓库，以便用户通过下方方式安装。   
+欢迎以 PR 或 issue 的形式投稿您的插件到[主程序文档](https://github.com/RockChinQ/QChatGPT#%E6%8F%92%E4%BB%B6%E7%94%9F%E6%80%81)
+
+下方是给用户看的内容，按需修改
+-->
+
+## 安装
+
+配置完成 [QChatGPT](https://github.com/RockChinQ/QChatGPT) 主程序后使用管理员账号向机器人发送命令即可安装：
+
+```
+!plugin get <插件发布仓库地址>
+```
+或查看详细的[插件安装说明](https://github.com/RockChinQ/QChatGPT/wiki/5-%E6%8F%92%E4%BB%B6%E4%BD%BF%E7%94%A8)
+
+## 使用
+
+<!-- 插件开发者自行填写插件使用说明 -->
---
+++ main.py
@@ -0,0 +1,49 @@
+from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
+from pkg.plugin.events import *  # 导入事件类
+
+
+# 注册插件
+@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
+class MyPlugin(BasePlugin):
+
+    # 插件加载时触发
+    def __init__(self, host: APIHost):
+        pass
+
+    # 异步初始化
+    async def initialize(self):
+        pass
+
+    # 当收到个人消息时触发
+    @handler(PersonNormalMessageReceived)
+    async def person_normal_message_received(self, ctx: EventContext):
+        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
+        if msg == "hello":  # 如果消息为hello
+
+            # 输出调试信息
+            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
+
+            # 回复消息 "hello, <发送者id>!"
+            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
+
+            # 阻止该事件默认行为（向接口获取回复）
+            ctx.prevent_default()
+
+    # 当收到群消息时触发
+    @handler(GroupNormalMessageReceived)
+    async def group_normal_message_received(self, ctx: EventContext):
+        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
+        if msg == "hello":  # 如果消息为hello
+
+            # 输出调试信息
+            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
+
+            # 回复消息 "hello, everyone!"
+            ctx.add_return("reply", ["hello, everyone!"])
+
+            # 阻止该事件默认行为（向接口获取回复）
+            ctx.prevent_default()
+
+    # 插件卸载时触发
+    def __del__(self):
+        pass
