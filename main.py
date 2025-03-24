1d55ca11815fe674dca42ecd97a0778b7025f6ed
Cheng-MaoMao <ctx13158034228@outlook.com>
2025-03-25 02:31:17 +0800

    修改版本号

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ class WaifuRunner(runner.RequestRunner):
         return
 
 
-@register(name="Waifu", description="Cuter than real waifu!", version="1.9.8", author="ElvisChenML")
+@register(name="Waifu", description="Cuter than real waifu!", version="1.9.9", author="ElvisChenML")
 class Waifu(BasePlugin):
     # 修改 __init__ 方法，初始化表情包管理器
     def __init__(self, host: APIHost):
